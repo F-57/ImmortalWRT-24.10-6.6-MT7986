fac481a7da6837c6b7fa44a5da7190900aa864a6
楠方 <80924478+F-57@users.noreply.github.com>
2025-09-15 18:47:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,8 +36,66 @@ sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set 
 # 24.10-6.6内核：名字看似是ubootmod，实则做了分区修改可以uboot放心刷入
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts
 
+# Theme
+git clone https://github.com/SAENE/luci-theme-design package/luci-theme-design
+git clone https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
+
+# 主题高级设置
+git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
+
 # 常用软件
+# 任务设置
 git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
+
+# 安装 mosdns
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
+rm -rf package/feeds/packages/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/feeds/packages/v2ray-geodata
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 安装 luci-app-openlist2 
+git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+
+# 安装隔空播放luci-app-airconnect
+git clone https://github.com/sbwml/luci-app-airconnect package/airconnect
+
+# 安装lucky
+git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
+
+# 安装 OpenClash
+git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
+rm -rf feeds/luci/applications/luci-app-openclash
+mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
+
+# 更改菜单名字
+echo -e "\nmsgid \"OpenClash\"" >> feeds/luci/applications/luci-app-openclash/po/zh-cn/openclash.zh-cn.po
+echo -e "msgstr \"科学上网\"" >> feeds/luci/applications/luci-app-openclash/po/zh-cn/openclash.zh-cn.po
+
+echo -e "\nmsgid \"MosDNS\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+echo -e "msgstr \"转发分流\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+
+echo -e "\nmsgid \"Lucky\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
+echo -e "msgstr \"大吉大利\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
+
+echo -e "\nmsgid \"OpenList\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
+echo -e "msgstr \"聚合网盘\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
+
+# 更改菜单
 sed -i 's/control/services/g' package/luci-app-taskplan/luci-app-taskplan/luasrc/controller/taskplan.lua
 sed -i 's/control/services/g' package/luci-app-taskplan/luci-app-taskplan/luasrc/view/taskplan/log.htm
 
+# 软件包与配置
+echo "CONFIG_CCACHE=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-kucat=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-advancedplus=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-taskplan=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-openlist2=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-lucky=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-airconnect=y" >> .config
