de4c70cf6d231fd6a24691defda4fe8b0293e424
楠方 <80924478+F-57@users.noreply.github.com>
2025-09-16 21:56:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -98,3 +98,4 @@ echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-lucky=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-airconnect=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-wechatpush=y" >> .config
