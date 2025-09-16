9b4a30b3048c2ddc9668908e290cbc6e9876bc6b
楠方 <80924478+F-57@users.noreply.github.com>
2025-09-17 00:09:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -82,6 +82,9 @@ echo -e "msgstr \"大吉大利\"" >> package/lucky/luci-app-lucky/po/zh_Hans/luc
 echo -e "\nmsgid \"OpenList\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
 echo -e "msgstr \"聚合网盘\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
 
+echo -e "\nmsgid \"UPnP\"" >> feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po
+echo -e "msgstr \"即插即用\"" >> feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po
+
 # 更改菜单
 #sed -i 's/control/services/g' package/luci-app-taskplan/luci-app-taskplan/luasrc/controller/taskplan.lua
 #sed -i 's/control/services/g' package/luci-app-taskplan/luci-app-taskplan/luasrc/view/taskplan/log.htm
