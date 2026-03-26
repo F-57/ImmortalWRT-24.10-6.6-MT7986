d8fc8bbb79bd1df3cf1d25638b2003ef4b39c04e
楠方 <80924478+F-57@users.noreply.github.com>
2026-03-26 19:38:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -94,8 +94,6 @@ echo -e "msgstr \"科学上网\"" >> feeds/luci/applications/luci-app-openclash/
 #echo -e "msgstr \"即插即用\"" >> feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po
 
 # 软件包与配置
-echo "CONFIG_CCACHE=y" >> .config
-
 echo "CONFIG_PACKAGE_luci-app-argon=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
 
