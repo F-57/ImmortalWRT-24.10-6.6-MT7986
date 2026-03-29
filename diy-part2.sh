a47d6e37b6bf1f1bc2f548c78d08b0ef300d7fbf
楠方 <80924478+F-57@users.noreply.github.com>
2026-03-29 19:35:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,7 +46,7 @@ git clone https://github.com/F-57/luci-app-adguardhome package/luci-app-adguardh
 
 # 安装应用过滤
 # rm -rf feeds/luci/applications/luci-app-appfilter
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # 安装 mosdns
 rm -rf feeds/packages/lang/golang
@@ -106,7 +106,7 @@ echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-openlist2=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-lucky=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-netwizard=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-oaf=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-appfilter=y" >> .config
 
 # 注入编译加速选项
 
