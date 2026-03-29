384bbf0629d91a6987c570416310a4912f336118
楠方 <80924478+F-57@users.noreply.github.com>
2026-03-30 00:33:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,62 +1,56 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-#主机变量
+# --- 主机变量设置 ---
 WRT_IP="10.0.0.1"
 WRT_NAME="AX6000"
-CFG_FILE="./package/base-files/files/bin/config_generate"
+CFG_FILE="package/base-files/files/bin/config_generate"
 
-#修改默认IP地址 修改默认主机名
+# 修改默认 IP 地址
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
-sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 
-#wifi相关变量
-WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
+# 修改默认主机名 (兼容不同源码格式)
+sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+sed -i "s/set system.@system\[0\].hostname=.*/set system.@system[0].hostname='$WRT_NAME'/g" $CFG_FILE
+
+
+# --- Wi-Fi 相关设置 (闭源驱动 mtwifi) ---
+WIFI_FILE="package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
 WIFI_SSID="Ax6000"
 WIFI_PASS="cw010203"
 
-#修改WIFI信道自动 WIFI名称 修改WIFI加密 修改WIFI密码
-sed -i "s/channel=.*/channel='auto'/g" $WIFI_FILE
-sed -i "s/ImmortalWrt/$WIFI_SSID/g" $WIFI_FILE
-sed -i "s/encryption=.*/encryption='sae-mixed'/g" $WIFI_FILE
-sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='$WIFI_PASS'" $WIFI_FILE
+if [ -f "$WIFI_FILE" ]; then
+    # 修改 Wi-Fi 信道为自动
+    sed -i "s/channel=.*/channel='auto'/g" $WIFI_FILE
+    # 修改默认 SSID (将默认的 ImmortalWrt 替换为你的变量)
+    sed -i "s/ImmortalWrt/$WIFI_SSID/g" $WIFI_FILE
+    # 修改加密方式为 WPA3/WPA2 混合 (sae-mixed)
+    sed -i "s/encryption=.*/encryption='sae-mixed'/g" $WIFI_FILE
+    # 在加密方式行后插入 Wi-Fi 密码
+    sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='$WIFI_PASS'" $WIFI_FILE
+    echo "Wi-Fi configuration updated."
+fi
 
-# 512布局
-# 24.10-5.4内核：
-# sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
-# 24.10-6.6内核：名字看似是ubootmod，实则做了分区修改可以uboot放心刷入
-sed -i 's/reg = <0x600000 0x[0-9a-fA-F]\{7\}>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts
+
+# --- 512MB 大分区布局适配 (ubootmod) ---
+# 针对 6.6 内核及更高版本：将起始地址设为 0x600000，长度设为 490MB (0x1ea00000)
+DTS_FILE="target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts"
+
+if [ -f "$DTS_FILE" ]; then
+    # 修改节点名称
+    sed -i 's/partition@.*/partition@600000 {/' $DTS_FILE
+    # 修改起始偏移与分区大小
+    sed -i 's/reg = <0x[0-9a-fA-F]* 0x[0-9a-fA-F]*>/reg = <0x600000 0x1ea00000>/' $DTS_FILE
+    echo "DTS partition (6MB Offset, 490MB Size) modified successfully."
+fi
+
 
 # Theme
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-git clone https://github.com/sbwml/luci-theme-argon -b openwrt-25.12 package/argon
+git clone https://github.com/sbwml/luci-theme-argon -b openwrt-24.10 package/argon
 
 # adguardhome
 git clone https://github.com/F-57/luci-app-adguardhome package/luci-app-adguardhome
-
-# 安装应用过滤
-# rm -rf feeds/luci/applications/luci-app-appfilter
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
-# 安装 mosdns
-rm -rf feeds/packages/lang/golang
-rm -rf feeds/packages/net/mosdns
-rm -rf package/feeds/packages/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf package/feeds/packages/v2ray-geodata
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 安装 luci-app-openlist2 
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
@@ -72,21 +66,9 @@ git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
 rm -rf feeds/luci/applications/luci-app-openclash
 mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
 
-#网络向导
-git clone https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
- 
 # 更改菜单名字
-#echo -e "\nmsgid \"KuCat Config\"" >> package/luci-app-kucat-config/po/zh_Hans/kucat-config.po
-#echo -e "msgstr \"主题设置\"" >> package/luci-app-kucat-config/po/zh_Hans/kucat-config.po
-
-echo -e "\nmsgid \"MosDNS\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
-echo -e "msgstr \"转发分流\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
-
 echo -e "\nmsgid \"OpenList\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
 echo -e "msgstr \"聚合网盘\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
-
-echo -e "\nmsgid \"Netwizard\"" >> package/luci-app-netwizard/luci-app-netwizard/po/zh_Hans/netwizard.po
-echo -e "msgstr \"向导\"" >> package/luci-app-netwizard/luci-app-netwizard/po/zh_Hans/netwizard.po
 
 echo -e "\nmsgid \"Lucky\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
 echo -e "msgstr \"大吉大利\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
@@ -98,18 +80,14 @@ echo -e "msgstr \"科学上网\"" >> feeds/luci/applications/luci-app-openclash/
 #echo -e "msgstr \"即插即用\"" >> feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po
 
 # 软件包与配置
-echo "CONFIG_PACKAGE_luci-app-argon=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-airconnect=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-openlist2=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-lucky=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-netwizard=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-appfilter=y" >> .config
 
 # 注入编译加速选项
-
 echo "CONFIG_CCACHE=y" >> .config
 echo "CONFIG_KERNEL_DEBUG_INFO=n" >> .config
 echo "CONFIG_OPENSSL_OPTIMIZE_SPEED=y" >> .config
