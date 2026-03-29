2f430c7e380a6096e7cdf6cc7cd724650f172872
楠方 <80924478+F-57@users.noreply.github.com>
2026-03-30 07:49:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,22 +36,10 @@ fi
 DTS_FILE="target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts"
 
 if [ -f "$DTS_FILE" ]; then
-    # 修改节点名称
-    sed -i 's/partition@.*/partition@600000 {/' $DTS_FILE
     # 修改起始偏移与分区大小
     sed -i 's/reg = <0x[0-9a-fA-F]* 0x[0-9a-fA-F]*>/reg = <0x600000 0x1ea00000>/' $DTS_FILE
-    echo "DTS partition (6MB Offset, 490MB Size) modified successfully."
 fi
 
-# --- 核心修复：彻底铲除 Rust 及其相关隐性依赖 ---
-# 这一步是解决你报错的关键：物理删除 feeds 里的 rust 源码
-rm -rf feeds/packages/lang/rust
-rm -rf feeds/packages/libs/librusty
-
-# 修正 SSR Plus 逻辑，防止其扫描 Rust 依赖
-if [ -d "package/feeds/luci/luci-app-ssr-plus" ]; then
-    sed -i '/default PACKAGE_librusty_v1_crypto/d' package/feeds/luci/luci-app-ssr-plus/Config.in
-fi
 
 # Theme
 rm -rf feeds/luci/themes/luci-theme-argon
