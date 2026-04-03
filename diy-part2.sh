d8726fb21e9e6079d96afba4dae8c43c89c0ca9c
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-03 22:30:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -116,7 +116,8 @@ echo "CONFIG_PACKAGE_luci-app-openlist2=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-lucky=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> .config
 
-# 注入编译加速选项
-echo "CONFIG_CCACHE=y" >> .config
+# 性能与体积优化
 echo "CONFIG_KERNEL_DEBUG_INFO=n" >> .config
 echo "CONFIG_OPENSSL_OPTIMIZE_SPEED=y" >> .config
+# 既然是 AX6000 512MB 版，建议再加一个提高插件运行效率的
+echo "CONFIG_STRIP_KERNEL_EXPORTS=y" >> .config
