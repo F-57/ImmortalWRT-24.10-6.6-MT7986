be0b38e985b01daf4b7aed3dcba636d2fff74b5f
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-19 05:20:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -89,6 +89,7 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" feeds/luci/applications/luci-app-u
 
 # 预置编译选项 (写入 .config)
 cat >> .config <<EOF
+CONFIG_CCACHE=y
 CONFIG_LUCI_LANG_en=y
 CONFIG_LUCI_LANG_zh_Hans=y
 CONFIG_KERNEL_DEBUG_INFO=n
