ec9019bae1c7aa7c19aba74842fff76eabc359a1
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-20 12:00:38 +0800

    Update diy-part2.sh

--- 25.12/diy-part2.sh
+++ 25.12/diy-part2.sh
@@ -96,7 +96,8 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" feeds/luci/applications/luci-app-u
 
 # 预置编译选项 (写入 .config)
 cat >> .config <<EOF
-CONFIG_CCACHE=y
+CONFIG_KERNEL_NET_CLS_CGROUP=y
+CONFIG_PACKAGE_kmod-zram=y
 CONFIG_LUCI_LANG_en=y
 CONFIG_LUCI_LANG_zh_Hans=y
 CONFIG_KERNEL_DEBUG_INFO=n
