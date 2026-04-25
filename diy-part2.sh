ae2265147a981141c6e6c47604fabf5b3ca1fb3b
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-26 02:14:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -95,7 +95,7 @@ change_name() {
 change_name "feeds/luci/modules/luci-base/po/zh_Hans/base.po" "Processes" "系统进程"
 change_name "feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po" "UPnP IGD & PCP" "端口映射"
 change_name "feeds/luci/applications/luci-app-openclash/po/zh-cn/openclash.zh-cn.po" "OpenClash" "科学上网"
-change_name "package/luci-app-lucky/po/zh_Hans/lucky.po" "Lucky" "万能工具"
+change_name "package/luci-app-lucky/po/zh_Hans/lucky.po" "Lucky" "路由助手"
 
 # 修改upnp服务地址
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" feeds/luci/applications/luci-app-upnp/htdocs/luci-static/resources/view/upnp/upnp.js
