b469f376dadf330202dce0ac338c739f048c17c6
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-26 02:04:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,12 +17,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # 自动查找luci-app-ttyd并清理，同时打印被修改的文件名
-echo "--- Cleaning up luci-app-ttyd dependencies ---"
-# 查找所有 Makefile，如果包含 +luci-app-ttyd 就打印并修改
-find ./package ./feeds -name Makefile -type f -exec grep -l "+luci-app-ttyd" {} + | while read -r file; do
-    echo "Patching: $file"
-    sed -i -E 's/[[:space:]]*\+luci-app-ttyd([[:space:]]|$)//g' "$file"
-done
-# 修改完后必须清理 tmp 目录，否则编译索引不会更新
-rm -rf tmp/
-echo "--- All done! ---"
+sed -i 's/+luci-app-ttyd //g' "$file"
