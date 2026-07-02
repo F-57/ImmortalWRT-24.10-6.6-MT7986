87cfaa8fa1bb57de84f498ca8edcc47420e1f640
楠方 <80924478+F-57@users.noreply.github.com>
2026-07-02 14:10:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -87,7 +87,7 @@ change_name "feeds/luci/modules/luci-base/po/zh_Hans/base.po" "Processes" "系�
 change_name "feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po" "UPnP IGD & PCP" "端口映射"
 change_name "feeds/luci/applications/luci-app-openclash/po/zh-cn/openclash.zh-cn.po" "OpenClash" "科学上网"
 change_name "package/luci-app-lucky/po/zh_Hans/lucky.po" "Lucky" "路由助手"
-change_name "package/luci-app-aurora-config/po/zh_Hans/aurora-config.po" "Aurora Theme Settings" "主题设置"
+change_name "package/luci-app-aurora-config/po/zh_Hans/aurora-config.po" "Aurora Settings" "主题设置"
 
 #修复Rust编译失败
 RUST_FILE=$(find feeds/packages/lang/rust/ -maxdepth 2 -type f -name "Makefile" 2>/dev/null)
